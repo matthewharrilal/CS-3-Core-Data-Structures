354622f738e694e56d8cfba756a429b04fe24fa9
matthewharrilal <matthewharrilal@gmail.com>
2018-01-12 11:07:27 -0800

    Finished the linear search recursive as well as iteratively

--- source/search.py
+++ source/search.py
@@ -7,17 +7,18 @@ def linear_search(array, item):
 
 
     index_of_item = 0
+
+    original_length = len(array)
     array_length = len(array)
 
     while array_length > 0:
         if array[index_of_item] == item:
-            return "The search was succesful here is you array: %s and item: %s" % (array, item)
-        if index_of_item > array_length:
+            return  (array, index_of_item)
+        if index_of_item > original_length:
             return None
         index_of_item += 1
         array_length -= 1
     return linear_search(array, item)
-# return linear_search_recursive(array, item)
 
 
 def linear_search_iterative(array, item):
@@ -30,9 +31,29 @@ def linear_search_iterative(array, item):
 
 def linear_search_recursive(array, item, index=0):
     # TODO: implement linear search recursively here
-    pass
     # once implemented, change linear_search to call linear_search_recursive
     # to verify that your recursive implementation passes all tests
+
+    # So first we grab the length of the array 
+    array_length = len(array)
+
+    # This is what we are doing to handle if the user passes in an item that does not exist with in the list
+    # of the array
+    if index > array_length:
+        return None
+
+    # This is handling our base case or what we are trying to work towards
+    if array[index] == item:
+        # If we have found it then return the array subscripted at that index
+        return array[index]
+
+    # For both the base case as well as the error case we have to increment the count due to the reason
+    # that if the item the user is looking for is not in the list then we increment the count because if it
+    # is not there then the index would exceed the length of the list as well as for its in there we still have to
+    #increment the count to go to the next element to see if it is in there
+    return linear_search_recursive(array,item, index + 1)
+
+
 
 
 def binary_search(array, item):
