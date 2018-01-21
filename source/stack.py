fc162e19e98d625ccb6aa681aa048e2302e963ad
matthewharrilal <matthewharrilal@gmail.com>
2018-01-20 17:50:14 -0800

    Moving on to queues

--- source/stack.py
+++ source/stack.py
@@ -56,8 +56,8 @@ class LinkedStack(object):
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return top item, if any
         # We want to account for the edge case if there is not items in the stack
-        if self.list.is_empty():
-            raise ValueError('Function undefined for empty stack')
+        if self.list.is_empty() == True:
+            raise ValueError
         else:
             latest_node = self.list.get_at_index(0)
             self.list.delete(latest_node)
@@ -76,6 +76,7 @@ class ArrayStack(object):
         """Initialize this stack and push the given items, if any."""
         # Initialize a new list (dynamic array) to store the items
         self.list = list()
+        self.size = 0
         if iterable is not None:
             for item in iterable:
                 self.push(item)
@@ -87,29 +88,63 @@ class ArrayStack(object):
     def is_empty(self):
         """Return True if this stack is empty, or False otherwise."""
         # TODO: Check if empty
+        if self.size == 0:
+            return True
+        return False
 
     def length(self):
         """Return the number of items in this stack."""
         # TODO: Count number of items
+        return self.size
 
     def push(self, item):
         """Insert the given item on the top of this stack.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Insert given item
+        self.list.append(item)
+        self.size += 1
 
     def peek(self):
         """Return the item on the top of this stack without removing it,
         or None if this stack is empty."""
         # TODO: Return top item, if any
+        if self.is_empty() == True:
+            return None
+        return self.list[self.size - 1]
 
     def pop(self):
         """Remove and return the item on the top of this stack,
         or raise ValueError if this stack is empty.
         Running time: O(???) – Why? [TODO]"""
         # TODO: Remove and return top item, if any
+        if self.is_empty() == True:
+            return None
+        latest_node = self.list[self.size - 1]
+        self.list.remove(latest_node)
+        self.size -= 1
+        return latest_node
 
 
 # Implement LinkedStack and ArrayStack above, then change the assignment below
 # to use each of your Stack implementations to verify they each pass all tests
 Stack = LinkedStack
 # Stack = ArrayStack
+Stack = ArrayStack
+
+def main():
+    import sys
+    args = sys.argv[1:]  # Ignore script file name
+    if len(args) == 0:
+        s = Stack()
+        # s = Stack(['A', 'B', 'C'])
+        # s = LinkedStack(['A', 'B', 'C'])
+        # print s.peek()
+        # print s.length()
+        print(s.is_empty())
+        # print s
+    else:
+        print('hello')
+
+
+if __name__ == '__main__':
+    main()
