06700ab1b01d639c002d16de8e0f0be53fd6c4bc
matthewharrilal <matthewharrilal@gmail.com>
2018-01-22 12:58:08 -0800

    Finished the append function for the doubly linked list

--- source/doubly_linked_list.py
+++ source/doubly_linked_list.py
@@ -41,7 +41,7 @@ class DoublyLinkedList(object):
             result_list.append(current_node.data)
 
             # We do this step so that we can continue iterating through the list
-            current_node = current_node.next
+            current_node = current_node.next_pointer
         return result_list
 
     def is_empty(self):
@@ -73,4 +73,30 @@ class DoublyLinkedList(object):
                 if counter == index:
                     return current_node.data
                 counter += 1
-                current_node = current_node.next
+                current_node = current_node.next_pointer
+
+    def append(self, item):
+        '''Appends an a node to the end of a linked list'''
+        new_node = BinaryNode(item)
+
+        # To save time complexity we first check if the list is empty
+        if self.is_empty() == True:
+            # If the list is empty then we want to set the head to the new node
+            self.head = new_node
+
+        else:
+            # Thats why they say that doubly linked lists are easier to work with therefore what is happening is that we
+            # are setting the node that comes before the new node to the tail
+            new_node.previous_pointer = self.tail
+            # The reason that we set the tail to the node before this node and then back again is because since we are
+            # saving time by going directly to the tail we are not keeping in mind the other nodes therefore we have to in
+            # a sense backtrack to the node before and keep that in memory where as opposed to when we are iterating
+            # through the linked list what we can do is that we can keep all of them in memory but that wastes more time
+
+            # And then to append to simply save time we want to go to the tail directly since we know that when appending
+            # we have to add right on to the end we can go to the tail directly and set its next pointer to the next node
+            self.tail.next_pointer = new_node
+        # Once we set the tails NEXT pointer to the new node we then set the tail to the new node
+        self.tail = new_node
+        # Since we are appending increase the size by 1
+        self.size += 1
