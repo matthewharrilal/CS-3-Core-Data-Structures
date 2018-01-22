ad647a2b42b403f703253c0219f2d269bcd9ab43
matthewharrilal <matthewharrilal@gmail.com>
2018-01-22 12:35:29 -0800

    Making progress on the Doubly Linked List Class

--- source/doubly_linked_list.py
+++ source/doubly_linked_list.py
@@ -30,3 +30,47 @@ class DoublyLinkedList(object):
     def __repr__(self):
         """Return a string representation of this linked list."""
         return 'LinkedList({!r})'.format(self.items())
+
+    def items(self):
+        '''Return a list of all items in the linked list'''
+        # This will not be different from the regular linked list because there is no need for the previous property
+        current_node = self.head
+        result_list = [] # Have a empty list so that we can contain all the nodes that we have iterated upon
+
+        while current_node is not None:
+            result_list.append(current_node.data)
+
+            # We do this step so that we can continue iterating through the list
+            current_node = current_node.next
+        return result_list
+
+    def is_empty(self):
+        '''Returns a boolean value indicating whether or not our linked list is empty or not'''
+        if self.size == 0:
+            return True
+        return False
+
+    def length_of_linked_list(self):
+        return self.size
+
+    def get_at_index(self, index):
+        '''Return item at given index or raise Value Error if list index is out of range or return None if list is empty'''
+
+        # Checks if the index is out of range of the linked list
+        if not (0 <= index < self.size):
+            raise ValueError('List index out of range: {}'.format(index))
+
+        # Checks if the linked list is empty if so returns None
+        if self.is_empty() == True:
+            return None
+        else:
+            current_node = self.head
+            counter = 0
+            if counter == index:
+                return current_node.data
+
+            while current_node is not None:
+                if counter == index:
+                    return current_node.data
+                counter += 1
+                current_node = current_node.next
