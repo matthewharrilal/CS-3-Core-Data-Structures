d7c39191324c2592b4a6a6f87f8493d9b5f8d15c
Matthew Harrilal <matthewharrilal@gmail.com>
2018-01-24 10:25:42 -0800

    Wrote the testing for the hash function now have to move on

--- source/Quadratic_Probing_Hash_Table.py
+++ source/Quadratic_Probing_Hash_Table.py
@@ -11,20 +11,18 @@ class Quadratic_Probing_Hash_Table(object):
 
     def hash_function(self, key):
         # Formulates hash number from given input
-        if type(key) != str or type(key) != int:
-            raise ValueError('Hash function undefined for key type other than string or integer')
         list_of_ascii_values = []
         if type(key) == str:
             # Find the ascii value for key if key is of type string
             for character in key:
                 list_of_ascii_values.append(ord(character))
-            ascii_key_value = int(''.join(sum(list_of_ascii_values)))
-            hash_number = (ascii_key_value + (self.counter ** 2)) % len(self.buckets)
+            ascii_key_value = (sum(list_of_ascii_values))
+            hash_number = (ascii_key_value + (self.counter ** 2))
             return hash_number
-        else:
-            hash_number = (key + (self.counter ** 2)) % len(self.buckets)
+        elif type(key) == int:
+            hash_number = (key + (self.counter ** 2))
             return hash_number
-
+        raise KeyError
 
     def _bucket_index(self, key):
         # Applies formula to find index where key value entry will be assigned
--- source/Quadratic_probing_test.py
+++ source/Quadratic_probing_test.py
@@ -7,3 +7,12 @@ class QuadraticProbingTesting(unittest.TestCase):
         assert len(ht.buckets) == 4
         assert ht.counter == 0
         assert ht.size == 0
+
+    def test_hash_function(self):
+        ht = Quadratic_Probing_Hash_Table()
+        assert ht.hash_function('hello') == 532
+        assert ht.hash_function('h e l l o') == 660
+        assert ht.hash_function(2) == 2
+        assert ht.hash_function(45) == 45
+        with self.assertRaises(KeyError):
+            assert ht.hash_function(45.74)
